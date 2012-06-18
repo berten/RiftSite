42dad147092a9c3ad96956476100b0358a5c12eb
Berten De Schutter <berten@ubuntu-Berten.(none)>
2012-06-18 16:11:14 +0200

    Added tests for dot/crits/heals

--- parser/src/test/java/org/deschutter/CombatAnalyzerIntegrationTest.java
+++ parser/src/test/java/org/deschutter/CombatAnalyzerIntegrationTest.java
@@ -42,6 +42,58 @@ public class CombatAnalyzerIntegrationTest {
         assertFalse(attackAction.isCriticalHit());
         assertEquals(new Integer(0), attackAction.getSecondsIntoFight());
         assertEquals("Lethal Poison", attackAction.getSkill());
+        assertEquals(new Integer(0), attackAction.getBlocked());
+        assertEquals(new Integer(0), attackAction.getOverkill());
+        assertEquals(new Integer(0), attackAction.getAbsorbed());
+    }
+    
+   @Test
+    public void dot_GetsParsed() throws Exception {
+        final List<Fight> fights = combatAnalyzer.analyze(new FileReader(System.getProperty("user.dir") + "/src/test/resources/dotAttack.txt"));
+
+        assertEquals(1, fights.size());
+        Fight fight = fights.get(0);
+
+        assertEquals(1, fight.getActions().size());
+        RiftAction action = fight.getActions().get(0);
+        assertTrue(action instanceof DamageDoneAction);
+        AttackAction attackAction = (AttackAction) action;
+
+        assertEquals("Nilus", attackAction.getActor());
+        assertEquals("Dreaded Longshot", attackAction.getTarget());
+        assertEquals(new Integer(232), attackAction.getAmount());
+        assertEquals(DamageTypeEnum.FIRE, attackAction.getDamageType());
+        assertFalse(attackAction.isCriticalHit());
+        assertEquals(new Integer(0), attackAction.getSecondsIntoFight());
+        assertEquals("Searing Vitality", attackAction.getSkill());
+        assertEquals(new Integer(0), attackAction.getBlocked());
+        assertEquals(new Integer(0), attackAction.getOverkill());
+        assertEquals(new Integer(0), attackAction.getAbsorbed());
+
+    }
+
+    @Test
+    public void critical_Attack_GetsParsed() throws Exception {
+        final List<Fight> fights = combatAnalyzer.analyze(new FileReader(System.getProperty("user.dir") + "/src/test/resources/criticalAttack.txt"));
+
+        assertEquals(1, fights.size());
+        Fight fight = fights.get(0);
+
+        assertEquals(1, fight.getActions().size());
+        RiftAction action = fight.getActions().get(0);
+        assertTrue(action instanceof DamageDoneAction);
+        AttackAction attackAction = (AttackAction) action;
+
+        assertEquals("Froesie", attackAction.getActor());
+        assertEquals("Rusila Dreadblade", attackAction.getTarget());
+        assertEquals(new Integer(2448), attackAction.getAmount());
+        assertEquals(DamageTypeEnum.EARTH, attackAction.getDamageType());
+        assertTrue(attackAction.isCriticalHit());
+        assertEquals(new Integer(0), attackAction.getSecondsIntoFight());
+        assertEquals("Pillaging Stone", attackAction.getSkill());
+        assertEquals(new Integer(0), attackAction.getBlocked());
+        assertEquals(new Integer(0), attackAction.getOverkill());
+        assertEquals(new Integer(0), attackAction.getAbsorbed());
 
     }
 
@@ -65,5 +117,73 @@ public class CombatAnalyzerIntegrationTest {
         assertEquals(new Integer(0), attackAction.getSecondsIntoFight());
         assertEquals("attack", attackAction.getSkill());
         assertEquals(new Integer(8623), attackAction.getBlocked());
+        assertEquals(new Integer(0), attackAction.getOverkill());
+        assertEquals(new Integer(0), attackAction.getAbsorbed());
+    }
+
+    @Test
+    public void criticalDamgeTaken_GetsParsed() throws Exception {
+        final List<Fight> fights = combatAnalyzer.analyze(new FileReader(System.getProperty("user.dir") + "/src/test/resources/criticalDamageTaken.txt"));
+
+        assertEquals(1, fights.size());
+        Fight fight = fights.get(0);
+
+        assertEquals(1, fight.getActions().size());
+        RiftAction action = fight.getActions().get(0);
+        assertTrue(action instanceof DamageTakenAction);
+        AttackAction attackAction = (AttackAction) action;
+
+        assertEquals("Dreaded Balemouth", attackAction.getActor());
+        assertEquals("Elfthryth", attackAction.getTarget());
+        assertEquals(new Integer(8865), attackAction.getAmount());
+        assertEquals(DamageTypeEnum.PHYSICAL, attackAction.getDamageType());
+        assertTrue(attackAction.isCriticalHit());
+        assertEquals(new Integer(0), attackAction.getSecondsIntoFight());
+        assertEquals("attack", attackAction.getSkill());
+        assertEquals(new Integer(0), attackAction.getBlocked());
+        assertEquals(new Integer(0), attackAction.getOverkill());
+        assertEquals(new Integer(0), attackAction.getAbsorbed());
+    }
+
+    @Test
+    public void normalHeal_GetsParsed() throws Exception {
+        final List<Fight> fights = combatAnalyzer.analyze(new FileReader(System.getProperty("user.dir") + "/src/test/resources/normalHeal.txt"));
+
+        assertEquals(1, fights.size());
+        Fight fight = fights.get(0);
+
+        assertEquals(1, fight.getActions().size());
+        RiftAction action = fight.getActions().get(0);
+        assertTrue(action instanceof HealingAction);
+        HealingAction healingAction = (HealingAction) action;
+
+        assertEquals("Whopper", healingAction.getActor());
+        assertEquals("Polkahunter", healingAction.getTarget());
+        assertEquals(new Integer(0), healingAction.getAmount());
+        assertFalse(healingAction.isCriticalHit());
+        assertEquals(new Integer(0), healingAction.getSecondsIntoFight());
+        assertEquals("Radiant Spores", healingAction.getSkill());
+        assertEquals(new Integer(2001), healingAction.getOverheal());
+    }
+
+    @Test
+    public void criticalHeal_GetsParsed() throws Exception {
+        final List<Fight> fights = combatAnalyzer.analyze(new FileReader(System.getProperty("user.dir") + "/src/test/resources/criticalHeal.txt"));
+
+        assertEquals(1, fights.size());
+        Fight fight = fights.get(0);
+
+        assertEquals(1, fight.getActions().size());
+        RiftAction action = fight.getActions().get(0);
+        assertTrue(action instanceof HealingAction);
+        HealingAction healingAction = (HealingAction) action;
+
+        assertEquals("Mimii", healingAction.getActor());
+        assertEquals("Arakki", healingAction.getTarget());
+        assertEquals(new Integer(2836), healingAction.getAmount());
+        assertTrue(healingAction.isCriticalHit());
+        assertEquals(new Integer(0), healingAction.getSecondsIntoFight());
+        assertEquals("Healing Invocation", healingAction.getSkill());
+        assertEquals(new Integer(3762), healingAction.getOverheal());
     }
 }
